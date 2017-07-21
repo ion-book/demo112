cc83ee24bcd5c40bd2351437eaa9b0fba69afc3b
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-07-21 19:48:12 -0400

    fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { UserService } from '../providers/user-service';
+import { UserService } from '../providers/user-service/user-service';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
